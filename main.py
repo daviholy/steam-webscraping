a7456131921dffab96ff4378eb7dc2c06b720937
daviholy <david.holy225@gmail.com>
2021-10-25 23:28:08 +0200

    added scraping info for top games from steamcharts

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import bs4
+
+PAGES=8
+if __name__ == "__main__":
+    names=[]
+    appID=[]
+    currentPlayers=[]
+    peakPlayers=[]
+    hoursPlayed=[]
+    for page in range(1,PAGES + 1):
+        response = requests.get("https://steamcharts.com/top/p." + str(page))
+        soup = bs4.BeautifulSoup(response.text, features="html.parser")
+        games = soup.find_all('tr')
+        games.pop(0)
+        for game in games:
+            gameName=game.find('td','game-name')
+            names.append(gameName.a.string.strip())
+            appID.append(gameName.a['href'].split('/')[2])
+            currentPlayers.append(int(game.find('td','num').string))
+            peakPlayers.append(int(game.find('td','peak-concurrent').string))
+            hoursPlayed.append(int(game.find('td','player-hours').string))
+    pass
