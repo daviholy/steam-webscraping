5978344c990b44a937c0a4be770dd4bf9c3977c6
daviholy <david.holy225@gmail.com>
2021-10-28 20:22:41 +0200

    implement scraping from steam and steamspy

--- main.py
+++ main.py
@@ -4,23 +4,45 @@ import pandas as pd
 import os
 import time
 import datetime
+from time import sleep
 
 from requests.api import request
 
-PAGES=8
+PAGES=1
 if __name__ == "__main__":
     names=[]
     appID=[]
 
+
+    #data from steamcharts
     gameNames=[]
     timeStamp=[]
     avgPlayers=[]
+    appIDS = []
     gain =[]
     gainper=[]
     peakpl=[]
+
+    #data from steam API
+    developers= []
+    publishers =[]
+    windows =[]
+    mac =[]
+    linux =[]
+    metacriticScore =[]
+
+    #data from SteamSpy
+    positiveRev = []
+    negativeRev = []
+    positiveRevPer = [] 
+    languages= []
+    genre = []
+    tags = []
     #scraping data from steamcharts
     for page in range(1,PAGES + 1):
         response = requests.get("https://steamcharts.com/top/p." + str(page))
+        if response.status_code != requests.codes.ok:
+            response.raise_for_status()
         soup = bs4.BeautifulSoup(response.text, features="html.parser")
         games = soup.find_all('tr')
         games.pop(0)
@@ -28,13 +50,17 @@ if __name__ == "__main__":
             gameName=game.find('td','game-name')
             names.append(gameName.a.string.strip())
             appID.append(gameName.a['href'].split('/')[2])
+    #scraping time data for scraped games
     for game in range(len(appID)):
         response = requests.get(f'https://steamcharts.com/app/{appID[game]}')
+        if response.status_code != requests.codes.ok:
+            response.raise_for_status()
         soup = bs4.BeautifulSoup(response.text, features="html.parser").tbody
         months = soup.find_all('tr')
         if len(months) > 1:
             data = months[0].find_all('td')
             gameNames.append(names[game])
+            appIDS.append(appID[game])
             timeStamp.append(int(time.mktime(datetime.datetime.strptime(f'{format(datetime.datetime.now().month,"02d")} {datetime.datetime.now().year}',"%m %Y").timetuple())))
             avgPlayers.append(float(data[1].text.strip()))
             gain.append(float(data[2].text.strip()))
@@ -44,6 +70,7 @@ if __name__ == "__main__":
                 for month in months[1:-1]:
                     data = month.find_all('td')
                     gameNames.append(names[game])
+                    appIDS.append(appID[game])
                     timeStamp.append(int(time.mktime(datetime.datetime.strptime(data[0].text.strip(),"%B %Y").timetuple())))
                     avgPlayers.append(float(data[1].text.strip()))
                     gain.append(float(data[2].text.strip()))
@@ -51,6 +78,7 @@ if __name__ == "__main__":
                     peakpl.append(int(data[4].text.strip()))
         data = months[-1].find_all('td')
         gameNames.append(names[game])
+        appIDS.append(appID[game])
         if len(months) >1:
             timeStamp.append(int(time.mktime(datetime.datetime.strptime(data[0].text.strip(),"%B %Y").timetuple())))
         else:
@@ -59,12 +87,56 @@ if __name__ == "__main__":
         peakpl.append(int(data[4].text.strip()))
         gain.append(0)
         gainper.append(0)
-
-        #creating dataframes
-    df = pd.DataFrame({'timeStamp':timeStamp,'gameName':gameNames,'averagePlayers':avgPlayers,'gain':gain ,'gainPercentage':gainper,'peakPlayers':peakpl})
+        print(f'\U00002714 top {PAGES * 25} games scraped from steamcharts.com')
+    #scraping from steam API
+    for game in appID:
+        response=requests.get("https://store.steampowered.com/api/appdetails",params={"appids": game})
+        if response.status_code != requests.codes.ok:
+            response.raise_for_status()
+        data =response.json()[str(game)]['data']
+        try:
+            developers.append(data['developers'])
+        except Exception:
+            developers.append({})
+        try:
+            publishers.append(data['publishers'])
+        except Exception:
+            publishers.append({})
+        try:
+            windows.append(data['platforms']['windows'])
+        except Exception:
+            windows.append(False)
+        try:
+            mac.append(data['platforms']['mac'])
+        except Exception:
+            mac.append(False)
+        try:
+            linux.append(data['platforms']['linux'])
+        except Exception:
+            linux.append(False)
+        try:
+            metacriticScore.append(data['metacritic']['score'])
+        except Exception:
+            metacriticScore.append(-1)
+        #scraping from steamSpy
+        response=requests.get("https://steamspy.com/api.php",params={"request" : "appdetails", "appids": game})
+        if response.status_code != requests.codes.ok:
+            response.raise_for_status()
+        data =response.json()
+        positiveRev.append(data['positive'])
+        negativeRev.append(data['negative'])
+        positiveRevPer.append(data['positive']/(data['positive'] + data['negative']))
+        languages.append(data['languages'].split(", "))
+        genre.append(data['genre'].split(", "))
+        tags.append(list(data['tags']))
+        sleep(1.5)
+    #creating dataframes
+    steam= pd.DataFrame({'appID': appID, 'developers':developers, 'publishers': publishers,'mac' :mac, 'linux' : linux, 'metacriticScore' : metacriticScore})
+    df = pd.DataFrame({'timeStamp':timeStamp,'gameName':gameNames, 'appID': appIDS,'averagePlayers':avgPlayers,'gain':gain ,'gainPercentage':gainper,'peakPlayers':peakpl})
+    steamSpy = pd.DataFrame({'appID' : appID, 'positive reviews':positiveRev, 'negative reviews':negativeRev, 'share of positive reviews' : positiveRevPer, 'supported languages':languages, 'genres' : genre, 'tags' : tags})
+    result = df.set_index('appID').join(steam.set_index('appID')).join(steamSpy.set_index('appID'))
     try:
         os.mkdir("ScrapedData")
     except Exception:
         pass
-    df.to_csv("ScrapedData/topGames.csv")
-    print(f'\U00002714 top {PAGES * 25} games scraped from steamcharts.com')
+    result.to_json("ScrapedData/topGames.csv", orient="records")
